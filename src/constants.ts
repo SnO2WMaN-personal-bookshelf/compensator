128eeb709fc550ad766f675765af3392bbab8285
SnO2WMaN <sno2wman.official@gmail.com>
2020-11-08 02:33:46 +0900

    feat: :sparkles: 画像を生成して配信

---
+++ src/constants.ts
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-anonymous-default-export */
+import path from 'path';
+
+export default {
+  fontFamily: 'mplus-1p-regular',
+  fontPath: path.resolve(process.cwd(), 'fonts', 'mplus-1p-regular.ttf'),
+  width: 340,
+  height: 480,
+  fontSize: 24,
+  backgroundColor: '#FFF',
+  textColor: '#000',
+};
---
+++ src/index.ts
@@ -0,0 +1,67 @@
+import {createCanvas, registerFont} from 'canvas';
+import fastify from 'fastify';
+import constants from './constants';
+
+const server = fastify({logger: true});
+
+export async function createImageBinary(title: string) {
+  const canvas = createCanvas(constants.width, constants.height);
+  const context = canvas.getContext('2d');
+
+  registerFont(constants.fontPath, {family: constants.fontFamily});
+
+  context.fillStyle = constants.backgroundColor;
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  context.fillStyle = constants.textColor;
+  context.font = `${constants.fontSize}px ${constants.fontFamily}`;
+
+  const box = {width: canvas.width * 0.75};
+
+  title
+    .split('')
+    .reduce(
+      (pre, char, i) => {
+        const wrap =
+          box.width <= pre[i].x + context.measureText(pre[i].char).width;
+        return [
+          ...pre,
+          {
+            char,
+            x: wrap
+              ? pre[0].x
+              : pre[i].x + context.measureText(pre[i].char).width,
+            y:
+              pre[i].y +
+              (wrap ? context.measureText(pre[i].char).emHeightAscent : 0),
+          },
+        ];
+      },
+      [{char: '', x: 0, y: 0}],
+    )
+    .forEach(({char, x, y}, _i, array) =>
+      context.fillText(
+        char,
+        (canvas.width - box.width) / 2 + x,
+        (canvas.height - (array[array.length - 1].y - array[0].y)) / 2 + y,
+      ),
+    );
+
+  return canvas.toBuffer();
+}
+
+server.get<{Querystring: {title: string}}>('/', async (request, reply) => {
+  const {title} = request.query;
+
+  const imageBuffer = await createImageBinary(title);
+
+  reply.type('image/png');
+  reply.header('Content-Length', imageBuffer.byteLength);
+  reply.header('Cache-Control', `max-age=2592000, public`);
+
+  return imageBuffer;
+});
+
+server.listen(3000, (err) => {
+  if (err) process.exit(1);
+});
